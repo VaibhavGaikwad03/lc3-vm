c6dbf1ae820f62519bdc6b136ec6cd1e5dd8f9fc
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-09-29 15:09:01 +0530

    Add files via upload

--- lc3.c
+++ lc3.c
@@ -84,6 +84,43 @@ void update_flags(uint16_t r)
         reg[R_COND] = FL_POS;
 }
 
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
+
+void read_image_file(FILE *file)
+{
+    // the origin tells us where in memory to place the image
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    // we know the maximum file size so we only need one fread
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t *p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    // swap to little endian
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        p++;
+    }
+}
+
+int read_image(const char *image_path)
+{
+    FILE *file = fopen(image_path, "rb");
+    if (!file)
+        return 0;
+
+    read_image_file(file);
+    fclose(file);
+
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     // load arguments
@@ -340,11 +377,28 @@ int main(int argc, char *argv[])
 
             case TRAP_PUTSP:
 
-                
+                // one char per byte (two bytes per word) here we need to swap back to big endian format
+                uint16_t *c = memory + reg[R_R0];
+
+                while (*c)
+                {
+                    char char1 = (*c) & 0xFF;
+                    putc(char1, stdout);
+                    char char2 = (*c) >> 8;
+                    if (char2)
+                        putc(char2, stdout);
+                    c++;
+                }
+                fflush(stdout);
 
                 break;
 
             case TRAP_HALT:
+
+                puts("HALT");
+                fflush(stdout);
+                running = 0;
+
                 break;
             }
 
